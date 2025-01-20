07da0daa5d6cb53f4ecedb02d1f8a87f9e6d1a15
Moazzam <moazzam.ali6659@gmail.com>
2025-01-20 09:50:46 +0530

    Create Signup functionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,43 +10,61 @@ import {
 } from './ui/dropdown-menu'
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
 import { LinkIcon, LogOut } from 'lucide-react'
+import { UrlState } from '@/Context'
+import customUseFetch from '@/hooks/Use-Fetch'
+import { logout } from '@/db/apiAuth'
+import { BarLoader } from 'react-spinners'
 
 const Header = () => {
   const navigate = useNavigate()
-  const user = false
+
+  const { user, fetchUser } = UrlState()
+
+  const { loading, fn: fnLogout } = customUseFetch(logout)
 
   return (
-    <nav className='py-4 flex items-center justify-between'>
-      <Link to='/'>
-        <img src='/logo.webp' alt='Logo' className='h-16' />
-      </Link>
+    <>
+      <nav className='py-4 flex items-center justify-between'>
+        <Link to='/'>
+          <img src='/logo.webp' alt='Logo' className='h-16' />
+        </Link>
 
-      <div>
-        {!user ? (
-          <Button onClick={() => navigate('/auth')}>Login</Button>
-        ) : (
-          <DropdownMenu>
-            <DropdownMenuTrigger className='outline-0 overflow-hidden rounded-full'>
-              <Avatar>
-                <AvatarImage src='https://github.com/shadcn.png' />
-                <AvatarFallback>MA</AvatarFallback>
-              </Avatar>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent>
-              <DropdownMenuLabel>Moazzam Ali</DropdownMenuLabel>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem>
-                <LinkIcon /> My Links
-              </DropdownMenuItem>
-              <DropdownMenuItem className='text-red-400'>
-                <LogOut />
-                <span>Logout</span>
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-        )}
-      </div>
-    </nav>
+        <div>
+          {!user ? (
+            <Button onClick={() => navigate('/auth')}>Login</Button>
+          ) : (
+            <DropdownMenu>
+              <DropdownMenuTrigger className='outline-0 overflow-hidden rounded-full'>
+                <Avatar>
+                  <AvatarImage src={user?.user_metadata?.profile_pic} className='object-contain' />
+                  <AvatarFallback>MA</AvatarFallback>
+                </Avatar>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuLabel>{user?.user_metadata?.name}</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem className='hover:cursor-pointer'>
+                  <LinkIcon /> My Links
+                </DropdownMenuItem>
+                <DropdownMenuItem className='text-red-400 hover:cursor-pointer'>
+                  <LogOut />
+                  <span
+                    onClick={() => {
+                      fnLogout().then(() => {
+                        fetchUser()
+                        navigate('/')
+                      })
+                    }}>
+                    Logout
+                  </span>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          )}
+        </div>
+      </nav>
+      {loading && <BarLoader className='mb-4' width={'100%'} color='#36d7b7' />}
+    </>
   )
 }
 
