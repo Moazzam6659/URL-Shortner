7ef04c44cfec4e58dc73ff1bf4261b3e1b9296f3
Moazzam <moazzam.ali6659@gmail.com>
2025-02-05 16:58:13 +0530

    Add redirect shot link to original link functionality and create single link detailed page with full analytics. Also add .htaccess file

---
+++ src/components/LocationStats.jsx
@@ -0,0 +1,30 @@
+import { LineChart, Line, XAxis, YAxis, Tooltip, Legend, ResponsiveContainer } from 'recharts'
+
+const LocationStats = ({ stats }) => {
+  const cityCount = stats.reduce((acc, item) => {
+    if (acc[item.city]) {
+      acc[item.city] += 1
+    } else acc[item.city] = 1
+
+    return acc
+  }, {})
+
+  const cities = Object.entries(cityCount).map(([city, count]) => ({
+    city,
+    count,
+  }))
+
+  return (
+    <ResponsiveContainer width={'100%'} height={300}>
+      <LineChart data={cities.slice(0, 5)} margin={{ top: 20 }} accessibilityLayer>
+        <XAxis dataKey='city' />
+        <YAxis />
+        <Tooltip labelStyle={{ color: 'green' }} />
+        <Legend />
+        <Line type='monotone' dataKey='count' stroke='#8884d8'></Line>
+      </LineChart>
+    </ResponsiveContainer>
+  )
+}
+
+export default LocationStats
