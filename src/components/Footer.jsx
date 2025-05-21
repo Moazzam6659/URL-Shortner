0163c67abfa843d8dc8cea04138390d7b443ba14
Moazzam <moazzam.ali6659@gmail.com>
2025-05-21 13:04:42 +0530

    Chnage footer name from Moazzam to Shahnawaz

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 const Footer = () => {
-  return <div className='p-10 text-center bg-gray-800 mt-10'>Made with ❤ by Moazzam.</div>
+  return <div className='p-10 text-center bg-gray-800 mt-10'>Made with ❤ by Shahnawaz.</div>
 }
 
 export default Footer
