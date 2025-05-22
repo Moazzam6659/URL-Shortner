affcf599832e9a6dfb25669b4da5e29c08ab5ed1
Moazzam <moazzam.ali6659@gmail.com>
2025-05-22 17:59:29 +0530

    Change Footer Heart Icon Color

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,9 @@
 const Footer = () => {
-  return <div className='p-10 text-center bg-gray-800 mt-10'>Made with ❤ by Moazzam.</div>
+  return (
+    <div className='p-10 text-center bg-gray-800 mt-10'>
+      Made with <span className='text-red-600'>❤</span> by Moazzam.
+    </div>
+  )
 }
 
 export default Footer
